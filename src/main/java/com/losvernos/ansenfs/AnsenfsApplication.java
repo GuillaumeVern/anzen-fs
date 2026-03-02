a0ccb391a7e4025a05cc59fc3706264468d7d7cb
Guillaume Vern <guillaume.vern@outlook.com>
2026-03-02 17:42:52 +0100

    test db connexion works

--- src/main/java/com/losvernos/ansenfs/AnsenfsApplication.java
+++ src/main/java/com/losvernos/ansenfs/AnsenfsApplication.java
@@ -1,14 +1,29 @@
 package com.losvernos.ansenfs;
 
+import java.sql.SQLException;
+
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.losvernos.ansenfs.database.DBAccess;
+import com.losvernos.ansenfs.database.DBInitializer;
+
+import jakarta.annotation.PostConstruct;
 
 @ImportRuntimeHints(AnsenfsApplication.WebResourcesHints.class)
+@RestController
 @SpringBootApplication
 public class AnsenfsApplication {
+
+	@PostConstruct
+    public void init() {
+        DBInitializer.initialize();
+    }
 
 	static class WebResourcesHints implements RuntimeHintsRegistrar {
         @Override
@@ -17,6 +32,19 @@ public class AnsenfsApplication {
             hints.resources().registerPattern("static/browser/**");
         }
     }
+
+	@RequestMapping("/backend")
+	String home() throws SQLException {
+		var conn = DBAccess.getConnection();
+		var stmt = conn.createStatement();
+		var rs = stmt.executeQuery("SELECT username FROM users");
+		StringBuilder sb = new StringBuilder();
+		while (rs.next()) {
+			sb.append(rs.getString("username")).append("\n");
+		}
+		return sb.toString();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnsenfsApplication.class, args);
---
+++ src/main/java/com/losvernos/ansenfs/database/DBAccess.java
@@ -0,0 +1,29 @@
+package com.losvernos.ansenfs.database;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBAccess {
+
+    private static final String DB_NAME = "ansen.db";
+    private static final String APP_NAME = "ansenfs";
+
+    public static Connection getConnection() {
+        String xdgDataHome = System.getenv("XDG_DATA_HOME");
+        if (xdgDataHome == null || xdgDataHome.isEmpty()) {
+            xdgDataHome = System.getProperty("user.home") + File.separator + ".local" + File.separator + "share";
+        }
+
+        File appDataDir = new File(xdgDataHome, APP_NAME);
+        File dbFile = new File(appDataDir, DB_NAME);
+
+        try {
+            var conn = DriverManager.getConnection("jdbc:sqlite:" + dbFile.getAbsolutePath());
+            return conn;
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to connect to database", e);
+        }
+    }
+}
